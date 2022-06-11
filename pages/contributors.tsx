a6e5e4e52d3f99647422bdbfe7ab920d2e0267ad
Marko Markovic <archorrs@gmail.com>
2022-06-11 10:43:14 +0200

    Add files via upload

--- pages/contributors.tsx
+++ pages/contributors.tsx
@@ -17,40 +17,39 @@
 import { GetStaticProps } from 'next';
 
 import Page from '@components/page';
-import SponsorsGrid from '@components/sponsors-grid';
-import Header from '@components/header';
+import SpeakersGrid from '@components/speakers-grid';
 import Layout from '@components/layout';
+import Header from '@components/header';
 
-import { getAllSponsors } from '@lib/cms-api';
-import { Sponsor } from '@lib/types';
+import { getAllSpeakers } from '@lib/cms-api';
+import { Speaker } from '@lib/types';
 import { META_DESCRIPTION } from '@lib/constants';
 
-type Props = { 
-  sponsors: Sponsor[];
+type Props = {
+  speakers: Speaker[];
 };
 
-export default function ExpoPage({ sponsors }: Props) {
+export default function Speakers({ speakers }: Props) {
   const meta = {
-    title: 'Expo - NFT marketplace built with the best',
+    title: 'Contributors - Community of NFT enthusiast from all around the world',
     description: META_DESCRIPTION
   };
-
   return (
     <Page meta={meta}>
       <Layout>
-        <Header hero="Expo" description={meta.description} />
-        <SponsorsGrid sponsors={sponsors} />
+        <Header hero="Contributors" description={meta.description} />
+        <SpeakersGrid speakers={speakers} />
       </Layout>
     </Page>
   );
 }
 
 export const getStaticProps: GetStaticProps<Props> = async () => {
-  const sponsors = await getAllSponsors();
+  const speakers = await getAllSpeakers();
 
   return {
     props: {
-      sponsors
+      speakers
     },
     revalidate: 60
   };
